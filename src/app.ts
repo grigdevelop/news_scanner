e850cfc52280f37c1bf3c13fba742199d4877dd1
Aleksanyan Grigor <grigdevelop@gmail.com>
2019-04-16 00:59:28 +0200

    refactor

--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,24 @@ import * as express from "express";
 import * as lessMiddleware from "less-middleware";
 import * as cookieParser from "cookie-parser";
 import * as bodyParser from 'body-parser';
+import * as path from 'path';
 
+// routes
+import scannerRoutes from './scanner';
 
 const app = express();
 const port : number = parseInt(process.env.PORT) || 4444;
-import * as path from 'path';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded()); 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname);
 
-import scannerRoutes from './scanner';
 
 app.use('/scanner', scannerRoutes);
 
